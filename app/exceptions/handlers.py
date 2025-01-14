78050c1f83ac1790ffd78c1e9fbd8f91231ea5fe
Zerohertz <ohg3417@gmail.com>
2025-01-14 22:02:08 +0900

    :recycle: refactor: exception handler (decorator pattern)

--- app/exceptions/handlers.py
+++ app/exceptions/handlers.py
@@ -1,9 +1,29 @@
 from fastapi import Request
+from fastapi.responses import JSONResponse
 from loguru import logger
+from starlette.status import HTTP_500_INTERNAL_SERVER_ERROR
 
-from app.core.decorators import exception
+from app.schemas.responses import APIResponse
 
 
-@exception
-def global_exception_handler(request: Request, exc: Exception) -> None:
+def global_exception_handler(request: Request, exc: Exception) -> JSONResponse:
     logger.error(f"{request=}, {exc=}")
+    name = exc.__class__.__name__
+    return JSONResponse(
+        content=APIResponse.error(
+            status=HTTP_500_INTERNAL_SERVER_ERROR,
+            message=f"""[{name}] {" ".join(exc.args)}""",
+        ).model_dump(mode="json"),
+        status_code=HTTP_500_INTERNAL_SERVER_ERROR,
+    )
+
+
+def business_exception_handler(request: Request, exc: Exception) -> JSONResponse:
+    logger.error(f"{request=}, {exc=}")
+    name = exc.__class__.__name__
+    return JSONResponse(
+        content=APIResponse.error(
+            status=exc.status, message=f"[{name}] {exc.message}"
+        ).model_dump(mode="json"),
+        status_code=exc.status,
+    )
--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ from fastapi import FastAPI
 from app.api.v1.routers import routers as v1_routers
 from app.core.configs import configs
 from app.core.lifespan import lifespan
-from app.exceptions.handlers import global_exception_handler
+from app.exceptions.base import BusinessException
+from app.exceptions.handlers import business_exception_handler, global_exception_handler
 
 app = FastAPI(
     title=configs.PROJECT_NAME,
@@ -11,7 +12,10 @@ app = FastAPI(
     openapi_url=f"{configs.PREFIX}/openapi.json",
     docs_url=f"{configs.PREFIX}/docs",
     redoc_url=f"{configs.PREFIX}/redoc",
-    exception_handlers={Exception: global_exception_handler},
+    exception_handlers={
+        Exception: global_exception_handler,
+        BusinessException: business_exception_handler,
+    },
     lifespan=lifespan,
 )
 

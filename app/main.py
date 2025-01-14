2ba59346cdcabc83b2e00874619cd53c2bf422f1
Zerohertz <ohg3417@gmail.com>
2025-01-14 21:22:48 +0900

    :recycle: refactor: deprecated function (related: #11)

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 import logging
+import sys
+from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, Request
 from loguru import logger
@@ -7,7 +9,13 @@ from app.api.v1.routers import routers as v1_routers
 from app.core.configs import configs
 from app.core.decorators import exception
 
-logging.getLogger("uvicorn.error").setLevel(logging.CRITICAL)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logging.getLogger("uvicorn.error").setLevel(logging.CRITICAL)
+    logger.remove()
+    logger.add(sys.stderr, colorize=True)
+    yield
 
 
 app = FastAPI(
@@ -16,19 +24,10 @@ app = FastAPI(
     docs_url=f"{configs.PREFIX}/docs",
     redoc_url=f"{configs.PREFIX}/redoc",
     version="0.0.1",
+    lifespan=lifespan,
 )
 for routers in [v1_routers]:
     app.include_router(routers, prefix=configs.PREFIX)
-
-
-@app.on_event("startup")
-async def startup():
-    pass
-
-
-@app.on_event("shutdown")
-async def shutdown():
-    pass
 
 
 @app.exception_handler(Exception)

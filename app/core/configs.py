9628d0f495419f92864b7a2daf2a771b1d0495ab
Zerohertz <ohg3417@gmail.com>
2025-01-16 23:43:10 +0900

    :hammer: add: database envs (related: #8)

--- app/core/configs.py
+++ app/core/configs.py
@@ -1,3 +1,5 @@
+from pydantic import AnyUrl, computed_field
+from pydantic_core import MultiHostUrl
 from pydantic_settings import BaseSettings
 
 
@@ -11,9 +13,22 @@ class Configs(BaseSettings):
     # --------- DATABASE SETTINGS --------- #
     DB_TYPE: str
     DB_HOST: str
+    DB_PORT: str
     DB_USER: str
     DB_PASSWORD: str
     DB_NAME: str
+
+    @computed_field
+    @property
+    def SQLMODEL_DATABASE_URI(self) -> AnyUrl:
+        return MultiHostUrl.build(
+            scheme=self.DB_TYPE,
+            host=self.DB_HOST,
+            port=self.DB_PORT,
+            username=self.DB_USER,
+            password=self.DB_PASSWORD,
+            path=self.DB_NAME,
+        )
 
 
 configs = Configs()  # type: ignore

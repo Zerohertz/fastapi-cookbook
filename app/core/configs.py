81afb35996c8f2a74accdc365ddaab21e5a4698c
Zerohertz <ohg3417@gmail.com>
2025-01-16 22:46:29 +0900

    :sparkles: feat: mysql (related: #8)

--- app/core/configs.py
+++ app/core/configs.py
@@ -2,10 +2,18 @@ from pydantic_settings import BaseSettings
 
 
 class Configs(BaseSettings):
+    # --------- APP SETTINGS --------- #
     PROJECT_NAME: str
     DESCRIPTION: str
     VERSION: str
     PREFIX: str
+
+    # --------- DATABASE SETTINGS --------- #
+    DB_TYPE: str
+    DB_HOST: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_NAME: str
 
 
 configs = Configs()  # type: ignore

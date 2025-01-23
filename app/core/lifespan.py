da8567e87d89edeca89ee9a570439536389d045f
Zerohertz <ohg3417@gmail.com>
2025-01-24 02:11:40 +0900

    :hammer: modify: logger level depends on environment

--- app/core/lifespan.py
+++ app/core/lifespan.py
@@ -6,7 +6,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from loguru import logger
 
-from app.core.configs import configs
+from app.core.configs import ENVIRONMENT, configs
 from app.core.container import Container
 from app.core.database import database
 from app.utils.logging import remove_handler
@@ -17,9 +17,12 @@ async def lifespan(app: FastAPI):  # pylint: disable=unused-argument
     remove_handler(logging.getLogger("uvicorn.access"))
     remove_handler(logging.getLogger("uvicorn.error"))
     logger.remove()
+    level = 0
+    if configs.ENV == ENVIRONMENT.PROD:
+        level = 20
     logger.add(
         sys.stderr,
-        level=0,
+        level=level,
         format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> <bg #800a0a>"
         + time.tzname[0]
         + "</bg #800a0a> | <level>{level: <8}</level> | <fg #800a0a>{name}</fg #800a0a>:<fg #800a0a>{function}</fg #800a0a>:<fg #800a0a>{line}</fg #800a0a> - <level>{message}</level>",

f4c8f1af228e2fe9ac087c56845dffcf8c1c14e7
Zerohertz <ohg3417@gmail.com>
2025-02-05 00:26:28 +0900

    :hammer: update: schema constraints (related: #36)

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -1,37 +1,38 @@
 from typing import Optional
 
+from pydantic import EmailStr, constr
+
 from app.models.enums import OAuthProvider, Role
 from app.schemas.base import BaseRequest, BaseResponse
 
 
 class UserRequest(BaseRequest):
-    name: str
-    # TODO: Email 검증
-    email: str
+    name: constr(min_length=3, max_length=30)
+    email: EmailStr
 
 
 class UserPatchRequest(BaseRequest):
-    name: Optional[str] = None
-    email: Optional[str] = None
-    password: Optional[str] = None
+    name: Optional[constr(min_length=3, max_length=30)] = None
+    email: Optional[EmailStr] = None
+    password: Optional[constr(min_length=8, max_length=30)] = None
 
 
 class UserResponse(BaseResponse):
-    name: str
-    email: str
+    name: constr(min_length=3, max_length=30)
+    email: EmailStr
     oauth: OAuthProvider
 
 
 class UserIn(UserRequest):
     role: Role
     oauth: OAuthProvider
-    password: Optional[str] = None
+    password: Optional[constr(min_length=8, max_length=30)] = None
     refresh_token: Optional[str] = None
     oauth_token: Optional[str] = None
 
 
 class UserOut(UserResponse):
     role: Role
-    password: Optional[str] = None
+    password: Optional[constr(min_length=8, max_length=30)] = None
     refresh_token: Optional[str] = None
     github_token: Optional[str] = None
